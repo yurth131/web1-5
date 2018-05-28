f5956ac0d38b921096c4750f0333e97aed787fd0
Unknown <g1727073@st.t-kougei.ac.jp>
2018-05-28 17:38:41 +0900

    最初のコミット

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speed = 0;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //スワイプの長さをもとめる
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos= Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            this.speed = swipeLength / 500.0f;
+
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+ 	}
+}
---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+
+    GameObject Car;
+    GameObject flag;
+    GameObject distance;
+
+    // Use this for initialization
+    void Start () {
+        this.Car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float length = this.flag.transform.position.x - this.Car.transform.position.x;
+        if (length >= 0) { this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m"; }
+        else
+        {
+            this.distance.GetComponent<Text>().text = "GAMEOVER";
+        }
+	}
+}
